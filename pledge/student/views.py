a0f2faf85a4574b17095c9993fd2bac2308e3d4b
asim3 <asimwebapps@gmail.com>
2020-12-10 15:27:27 +0300

    update PledgeView

--- pledge/student/views.py
+++ pledge/student/views.py
@@ -1,13 +1,15 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, FormView
+from django.urls import reverse_lazy
+from .forms import PledgeForm
+from django.contrib.messages.views import SuccessMessageMixin
 
 
 class HomeView(TemplateView):
     template_name = "student/home.html"
 
 
-class PledgeView(TemplateView):
+class PledgeView(SuccessMessageMixin, FormView):
     template_name = "student/pledge.html"
-
-
-class LoginView(TemplateView):
-    template_name = "student/login.html"
+    form_class = PledgeForm
+    success_url = reverse_lazy("home")
+    success_message = "was created successfully"
