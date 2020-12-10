a1c4db129d6f7da758104cb2e21af0b9cc90b733
asim3 <asimwebapps@gmail.com>
2020-12-10 12:03:51 +0300

    adding administration StudentView

--- pledge/administration/views.py
+++ pledge/administration/views.py
@@ -1,4 +1,10 @@
 from django.views.generic import TemplateView
 
-class HomeView(TemplateView):
-    template_name = "home.html"
+
+class StudentView(TemplateView):
+    """
+    List all student registered in the database, 
+    add new students using an excel file,
+    download the template excel file.
+    """
+    template_name = "administration/student.html"
--- pledge/pledge/urls.py
+++ pledge/pledge/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from student.views import HomeView
+from administration.views import StudentView
 
 urlpatterns = [
+    path('admin/student', StudentView.as_view()),
     path('admin/', admin.site.urls),
-    path('', HomeView.as_view() ),
+    path('', HomeView.as_view()),
 ]
