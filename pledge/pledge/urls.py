c3c1bdbb126bbe01fc392192be92b360949a40bb
asim3 <asimwebapps@gmail.com>
2020-12-10 12:26:20 +0300

    add PledgeView, LoginView

--- pledge/pledge/urls.py
+++ pledge/pledge/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-from student.views import HomeView
+from student.views import HomeView, PledgeView, LoginView
 from administration.views import StudentView
 
 urlpatterns = [
-    path('admin/student', StudentView.as_view()),
+    path('login/', LoginView.as_view()),
+    path('pledge/', PledgeView.as_view()),
+    path('admin/student/', StudentView.as_view()),
     path('admin/', admin.site.urls),
     path('', HomeView.as_view()),
 ]
--- pledge/student/views.py
+++ pledge/student/views.py
@@ -1,4 +1,13 @@
 from django.views.generic import TemplateView
 
+
 class HomeView(TemplateView):
     template_name = "student/home.html"
+
+
+class PledgeView(TemplateView):
+    template_name = "student/pledge.html"
+
+
+class LoginView(TemplateView):
+    template_name = "student/login.html"
