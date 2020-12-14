7a5d878d40a114dcd1d801750e732c1a94f52ab7
asim3 <asimwebapps@gmail.com>
2020-12-14 13:02:52 +0300

    update run-me to V2

--- run-me-v2.sh
+++ run-me-v2.sh
@@ -16,11 +16,17 @@ elif [ "$1" = "newapp" ]; then
   python3 manage.py startapp $2
 elif [ "$1" = "test" ]; then
   python3 manage.py test $2;
+elif [ "$1" = "shell" ]; then
+  python3 manage.py shell
+elif [ "$1" = "user" ]; then
+  python3 manage.py createsuperuser
 elif [ "$1" = "help" ]; then
   echo "You can use:"
   echo "   ./run-me.sh install"
   echo "   ./run-me.sh newapp my_app"
   echo "   ./run-me.sh test"
+  echo "   ./run-me.sh shell"
+  echo "   ./run-me.sh user"
   echo "   ./run-me.sh"
 else
   python3 manage.py runserver
