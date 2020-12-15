d06b4c444548a7c1e3a807b79cc511b8909e1c30
asim3 <asimwebapps@gmail.com>
2020-12-15 16:21:50 +0300

    fix LogoutView

--- pledge/pledge/urls.py
+++ pledge/pledge/urls.py
@@ -1,15 +1,16 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, reverse_lazy
 from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls.static import static
 from django.conf import settings
 
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from student.views import HomeView, PledgeView
 
 
 urlpatterns = i18n_patterns(
     path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(next_page=reverse_lazy('home')), name='logout'),
     path('pledge/', PledgeView.as_view(), name='pledge'),
     path('admin/', admin.site.urls),
     path('', HomeView.as_view(), name='home'),
