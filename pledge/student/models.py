b0a54d70aa80ad9207849d1174ecdf26dbd959c0
asim3 <asimwebapps@gmail.com>
2020-12-28 12:28:01 +0300

    update PledgeType choices

--- pledge/student/models.py
+++ pledge/student/models.py
@@ -1,5 +1,5 @@
 from django.db.models import (
-    Model, CASCADE, BooleanField,
+    Model, CASCADE, BooleanField, TextChoices,
     IntegerField, CharField, DecimalField, ForeignKey, DateTimeField,)
 from django.contrib.auth.models import User
 from django.utils.translation import gettext_lazy as _
@@ -12,23 +12,15 @@ class Pledge(Model):
         verbose_name = _('Pledge')
         verbose_name_plural = _("Pledges")
 
-    class PledgeType:
-        CONDITION = 'S'
-        FINAL = 'S1'
-        DISMISS = 'D'
-
-        @classmethod
-        def choices(cls):
-            return (
-                (cls.CONDITION, _('condition')),
-                (cls.FINAL, _('final')),
-                (cls.DISMISS, _('dismiss')),
-            )
+    class PledgeType(TextChoices):
+        CONDITION = 'S', _('condition')
+        FINAL = 'S1', _('final')
+        DISMISS = 'D', _('dismiss')
 
     student = ForeignKey(User, on_delete=CASCADE, verbose_name=_('student'))
     pledge_type = CharField(_('Pledge Type'),
                             max_length=2,
-                            choices=PledgeType.choices())
+                            choices=PledgeType.choices)
     next_tearm = IntegerField(_('Next Tearm'), )
     kfupm_gpa = DecimalField(_('KFUPM GPA'),
                              max_digits=3, decimal_places=2, blank=True, null=True)
