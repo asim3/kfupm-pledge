3db2b2a1f96dd865930c3cae399e7f3453ac40c1
asim3 <asimwebapps@gmail.com>
2020-12-29 22:45:06 +0300

    add student in background using threading

--- pledge/administration/models.py
+++ pledge/administration/models.py
@@ -4,8 +4,43 @@ from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.models import User
 from openpyxl import load_workbook
 from student.models import Pledge
+from threading import Thread
 
 
+def extract_excel_file(excel_file_path):
+    excel_file = load_workbook(excel_file_path)
+    for sheet in excel_file.sheetnames:
+        for (i, row) in enumerate(excel_file[sheet].rows):
+            if i < 1:
+                continue
+            add_new_student(row, sheet.capitalize())
+    
+def add_new_student(excel_row, pledge_type):
+    kfupmid = str(excel_row[1].value)
+    try:
+        user = User.objects.get(username=kfupmid)
+    except User.DoesNotExist:
+        user = User.objects.create_user(
+            kfupmid, 
+            email=f's{kfupmid}@kfupm.edu.sa',
+            password=str(excel_row[2].value), 
+            first_name=excel_row[3].value)
+        
+    next_tearm = excel_row[4].value
+    kfupm_gpa = excel_row[5].value if len(excel_row) > 5 else 0.0
+    try:
+        Pledge.objects.get(
+            student=user,
+            pledge_type=pledge_type,
+            next_tearm=next_tearm)
+        print('get')
+    except Pledge.DoesNotExist:
+        Pledge.objects.create(
+            student=user,
+            pledge_type=pledge_type,
+            kfupm_gpa=kfupm_gpa,
+            next_tearm=next_tearm)
+        print('add')
 
 
 class AddStudents(Model):
@@ -20,39 +55,6 @@ class AddStudents(Model):
     def get_absolute_url(self):
         return reverse_lazy('admin-student')
 
-        
-    def add_new_student(self, excel_row, pledge_type):
-        kfupmid = str(excel_row[1].value)
-        try:
-            user = User.objects.get(username=kfupmid)
-        except User.DoesNotExist:
-            user = User.objects.create_user(
-                kfupmid, 
-                email=f's{kfupmid}@kfupm.edu.sa',
-                password=str(excel_row[2].value), 
-                first_name=excel_row[3].value)
-            
-        next_tearm = excel_row[4].value
-        kfupm_gpa = excel_row[5].value if len(excel_row) > 5 else 0.0
-        try:
-            Pledge.objects.get(
-                student=user,
-                pledge_type=pledge_type,
-                next_tearm=next_tearm)
-            print('get')
-        except Pledge.DoesNotExist:
-            Pledge.objects.create(
-                student=user,
-                pledge_type=pledge_type,
-                kfupm_gpa=kfupm_gpa,
-                next_tearm=next_tearm)
-            print('add')
-
     def save(self, *args, **kwargs):
-        response = super().save(*args, **kwargs)
-        excel_file = load_workbook(self.excel_file.path)
-        for sheet in excel_file.sheetnames:
-            for (i, row) in enumerate(excel_file[sheet].rows):
-                if i < 1:
-                    continue
-                self.add_new_student(row, sheet.capitalize())
+        super().save(*args, **kwargs)
+        Thread(target=extract_excel_file, args=(self.excel_file.path,)).start()
